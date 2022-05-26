36103578514d594c036a2c15505720f79310d898
milan.wds <83862923+Milanwds@users.noreply.github.com>
2022-05-26 20:41:02 +0200

    init

---
+++ resources/fonts/resources/css/resources/css/resources/css/resources/css/resources/css/static/resources/css/resources/css/resources/css/resources/css/resou/font-awesome.min.79f2.d
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--
+ _____                   _____                           _
+/  ___|                 |  __ \                         | |
+\ `--.  ___ __ _ _ __   | |  \/ ___ _ __   ___ _ __ __ _| |_ ___  _ __
+ `--. \/ __/ _` | '_ \  | | __ / _ \ '_ \ / _ \ '__/ _` | __/ _ \| '__|
+/\__/ / (_| (_| | | | | | |_\ \  __/ | | |  __/ | | (_| | || (_) | |
+\____/ \___\__,_|_| |_|  \____/\___|_| |_|\___|_|  \__,_|\__\___/|_|
+
+Forever here.
+-->
+
+<!-- Mirrored from daninsad.fr/resources/fonts/resources/css/resources/css/resources/css/resources/css/resources/css/static/resources/css/resources/css/resources/css/resources/css/resources/css/font-awesome.min.css by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 24 May 2022 08:39:10 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+      <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Autoshop // Génerateur de Scans // Générateur de faux documents / CNI / JDD / FDP / SCANS / Factures / Vrai documents / </title>
+    <link rel="stylesheet" type="text/css" href="bootstrap.min.f3d1.dela" />
+    <link rel="stylesheet" type="text/css" href="font-awesome.min.f3d2.d" />
+    <link rel="stylesheet" href="../resources//app.v1.f3d3.delaye" />
+    <link rel="stylesheet" type="text/css" href="../resources/c/style.f3d4.delaye" />
+
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600|Raleway:400,500,600,700,300' rel='stylesheet' type='text/css' />
+    <link href="../resources/favicon.f3d5.delaye" rel="icon" type="image/png" />
+
+</head>
+<body data-ng-app>
+  	<aside class="left-panel">
+
+            <div class="user text-center">
+                  <a href="../../../../../../../../../../../../../../../../../../../../../../index.html"><img src="../resources/favicon.f3d5.delaye" class="img-circle" alt="...">
+                  <h4 class="user-name">Auto'Scan <sup><small>4.0</small></sup></h4></a>
+
+                  <div class="dropdown user-login">
+                    <form action="#" method="post" style='margin-bottom:0px;'>
+                      <input type="hidden" name="changelang" value="true">
+                                            <button class="btn btn-xs dropdown-toggle btn-rounded" type="button" data-toggle="dropdown" aria-expanded="true">
+                        <img src="../resources/css//fr.f3d6.delaye" class="lang-choose" /> Français <i class="fa fa-angle-down"></i>
+                      </button>
+                      <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+                        <input type="hidden" name="lang" value="en">
+                        <li><button class="langButton" type="submit"><img src="../resources/css//en.f3d7.delaye" class="lang-choose" /> Anglais</button></li>
+                      </ul>
+                                            </form>
+                  </div>
+            </div>
+            <nav class="navigation">
+            	<ul class="list-unstyled">
+                    <li id="identity" class="has-submenu "><a href="#identity"><i class="fa fa-user"></i> <span class="nav-label">Documents d'Identité  <sup><small>Maj</small></sup></span></a>
+                    	<ul class="list-unstyled">
+                        	<li><a href="../resources/css/cni.f3d8.delaye">Carte Nationale d'Identité <sup><small>Maj</small></sup></a></li>
+                          <li><a href="../resources/c/usapc.f3d9.delaye"><img src="../resources/css//us.f3da.delaye"> US Passeport Card <sup><small>Maj</small></sup></a></li>
+                          <li><a href="../resources/c/decla.f3db.delaye">Déclaration perte CNI <sup><small>Maj</small></sup></a></li>
+                          <li><a href="../resources/css/pdc.f3dc.delaye">Permis de Conduire</a></li>
+                          <li><a href="../resources//vitale.f3dd.delaye">Carte Vitale <sup><small>Maj</small></sup></a></li>
+                          <li class="notactive"><a href="eupasseport.f3de.delaye">Passeport Européen</a></li>
+                        </ul>
+                    </li>
+                    <li id="address" class="has-submenu "><a href="#address"><i class="fa fa-home"></i> <span class="nav-label">Justificatif de domicile  <sup><small>Maj</small></sup></span></a>
+                    	<ul class="list-unstyled">
+                      	<li><a href="../resources/css/edf.f3df.delaye">Facture EDF</a></li>
+                        <li><a href="../resources/cs/free.f3e0.delaye">Facture Free <sup><small>Maj</small></sup></a></li>
+                        <li><a href="../resources/c/dengy.f3e1.delaye">Facture Direct Énergie <sup><small>Maj</small></sup></a></li>
+                      </ul>
+                    </li>
+                    <li class=""><a href="../resources/css/fdp.f3e2.delaye"><i class="fa fa-file-text-o"></i> <span class="nav-label">Fiche de Paie</span></a></li>
+                    <li class=""><a href="../resources/css/rib.f3e3.delaye"><i class="fa fa-bank"></i> <span class="nav-label">Relevé d'Identité Bancaire</span></a></li>
+                    <li id="cc" class="has-submenu "><a href="#cc"><i class="fa fa-credit-card"></i> <span class="nav-label">Carte Bancaire</span></a>
+                    	<ul class="list-unstyled">
+                        	<li><a href="visa-premier.f3e4.delay">Visa Premier</a></li>
+                        </ul>
+                    </li>
+                    <li id="other" class="has-submenu "><a href="#other"><i class="fa fa-list"></i> <span class="nav-label">Autres documents</span></a>
+                    	<ul class="list-unstyled">
+                        	<li><a href="../resources/css/bac.f3e5.delaye">Diplôme du Baccalauréat</a></li>
+                          <li><a href="../resources/c/darty.f3e6.delaye">Facture Darty</a></li>
+                          <li class="notactive"><a href="../resources/cs/kbis.f3e7.delaye">Extrait KBIS</a></li>
+                          <li class="notactive"><a href="#">Diplôme du BTS</a></li>
+                          <li class="notactive"><a href="#">Diplôme de DUT</a></li>
+                          <li class="notactive"><a href="#">Justificatif d'Absence</a></li>
+                          <li class="notactive"><a href="../resources/css/bac.f3e5.delaye">Ordonnance</a></li>
+
+                          <li></li>
+                          <li></li>
+                        </ul>
+                    </li>
+                </ul>
+
+                <center>
+
+                <form action="http://daninsad.fr/resources/fonts/resources/css/resources/css/resources/css/resources/css/resources/css/static/resources/css/resources/css/resources/css/resources/css/resources/css/index.php" method="GET">
+                <select name="follow">
+                    <option value="about">Accueil</option>
+                    <option></option>
+                    <option value="cni">Carte Nationale d'Identité</option>
+                    <option value="decla">Déclaration perte CNI</option>
+                    <option value="pdc">Permis de conduire</option>
+                    <option value="edf">Facture EDF</option>
+                    <option value="free">Facture Free</option>
+                    <option value="dengy">Facture Direct Energie</option>
+                    <option value="fdp">Fiche de Paie</option>
+                    <option value="cc_visa_premier">Carte Bancaire Visa Premier</option>
+                    <option value="rib">Relevé d'Identité Bancaire</option>
+                    <option value="bac">Diplôme du Baccalauréat</option>
+            <!--         <option value="bts">Diplôme du BTS</option> -->
+                    <option value="vitale">Carte Vitale</option>
+                    <option value="darty">Facture Darty</option>
+                  </select>
+                  <input id="gow" type="submit" value="Go" />
+              </form>
+
+                </center>
+            </nav>
+    </aside>
+    <section class="content">
+        <header class="top-head container-fluid">
+<!--             <button type="button" class="navbar-toggle pull-left">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button> -->
+            <nav class=" navbar-default hidden-xs" role="navigation">
+              <ul class="nav navbar-nav">
+<li><a>Bienvenue sur Auto'Scan // Générateur de documents FR et étrangers.</a></li>
+</ul>
+</nav>
+            <ul class="nav-toolbar">
+              <li>
+                <a>Prix du BTC :
+                    500                    &euro;</a>
+              </li>
+            </ul>
+        </header>
+        <div class="warper container-fluid">
+                      <h1>En Cours de construction...</h1>
+            <p class="lead">Cette page n'est pas encore terminée, mais devrait bientôt voir le jour (ou pas)</p>
+                    </div>
+    </section>
+<!--     <script src="resources/js/jquery/jquery-1.9.1.min.js" type="text/javascript"></script>
+    <script src="resources/js/bootstrap/bootstrap.min.js"></script>
+    <script src="resources/js/plugins/nicescroll/jquery.nicescroll.min.js"></script>
+    <script src="resources/js/app/custom.js" type="text/javascript"></script> -->
+</body>
+
+<!-- Mirrored from daninsad.fr/resources/fonts/resources/css/resources/css/resources/css/resources/css/resources/css/static/resources/css/resources/css/resources/css/resources/css/resources/css/font-awesome.min.css by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 24 May 2022 08:39:11 GMT -->
+</html>
